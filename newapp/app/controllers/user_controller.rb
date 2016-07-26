56fe040cebebcabfcde1bfdb761da4ef934829db
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-07-26 13:00:56 -0700

    integrate modal with 3 stacks of forms

--- newapp/app/controllers/user_controller.rb
+++ newapp/app/controllers/user_controller.rb
@@ -12,7 +12,7 @@ class UserController < ApplicationController
     if user.save
       flash[:success] = "registration successful"
       session[:user_id] = user.id
-      redirect_to root_path
+       redirect_to root_path
     else
        flash[:danger] = "Please enter a valid email and or password"
       redirect_to '/user/new'
