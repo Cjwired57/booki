5b20f160745cf520fa6eb2bf445cfc5005114a98
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-08-05 14:51:37 -0700

    user can accept requests and create bets

--- newapp/app/controllers/bet_controller.rb
+++ newapp/app/controllers/bet_controller.rb
@@ -1,2 +1,13 @@
 class BetController < ApplicationController
+	def create
+		p "you hit the bet post"
+		Bet.create(request_id: params[:request_id])
+		request = Request.find(params[:request_id])
+		request.accepted = true
+		request.save
+		redirect_to request_index_path
+	end
+      # t.integer :request_id
+      # t.integer :winner_id
+
 end
