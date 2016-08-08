14e7f77fb54953d134e8789b13608ae67add24fa
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-08-07 21:01:15 -0700

    charge both participants of bet on creation

--- newapp/app/controllers/bet_controller.rb
+++ newapp/app/controllers/bet_controller.rb
@@ -1,4 +1,7 @@
+require "stripe"
+
 class BetController < ApplicationController
+	Stripe.api_key = "sk_test_LaGOwlDvqeTDlyYRBnWN1y4W"
 
 	def index
 		@user = current_user
@@ -8,10 +11,29 @@ class BetController < ApplicationController
 
 	def create
 		p "you hit the bet post"
+
+
 		Bet.create(request_id: params[:request_id])
 		request = Request.find(params[:request_id])
 		request.accepted = true
 		request.save
+
+		@opponent = User.find(request.opponent_id)
+		@owner = User.find(request.owner_id)
+		@bet_amount = request.bet_amount_in_cents
+
+		Stripe::Charge.create(
+	      amount: @bet_amount,
+	      currency: 'usd',
+	      customer: @owner.customer_id
+      )
+
+		Stripe::Charge.create(
+	      amount: @bet_amount,
+	      currency: 'usd',
+	      customer: @opponent.customer_id
+      )
+
 		redirect_to request_index_path
 	end
       # t.integer :request_id
